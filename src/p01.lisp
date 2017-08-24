9a1af4b66a89b25fc5dfa0e82564517b86402062
Aleksandr Block <aleksandr.block@gmail.com>
2017-08-24 08:29:10 +0300

    p01.lisp update

--- src/p01.lisp
+++ src/p01.lisp
@@ -1,6 +1,7 @@
 ;; list -> list
 ;; example: (my-last '(a b c d)) -> (D)
 (defun my-last (lst)
-  (if (not (cdr lst))
-      lst
-      (my-last (cdr lst))))
+  (and (consp lst) 
+       (if  (not (cdr lst))
+	    lst
+	    (my-last (cdr lst)))))
